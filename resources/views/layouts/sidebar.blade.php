a6b2dd3339419059cc8fdacee65acc4936a2a494
Ismael Gaudioso <ismaelgaudioso@gmail.com>
2023-04-11 12:13:55 +0200

    Logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div>
         <div class="-mx-6 px-6 py-4">
             <a href="#" title="home">
-                <img src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg" class="w-32" alt="tailus logo">
+                <img src="{{asset('images/logo.png')}}" class="w-32 ml-10" alt="mygaragehq logo">
             </a>
         </div>
 
